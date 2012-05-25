f35907872ee4618f5e1c202f8c0a04e8e8cecaf5
franciscofranco <franciscofranco.1990@gmail.com>
2012-05-25 02:06:01 +0100

    Fix wifi_pm module not being loaded on module load.

--- drivers/net/wireless/bcmdhd/src/dhd/sys/dhd_linux.c
+++ drivers/net/wireless/bcmdhd/src/dhd/sys/dhd_linux.c
@@ -73,10 +73,6 @@
 #include <linux/time.h>
 #include <htsf.h>
 
-int wifi_pm = 0;
-module_param(wifi_pm, int, 0755);
-EXPORT_SYMBOL(wifi_pm);
-
 #define HTSF_MINLEN 200    /* min. packet length to timestamp */
 #define HTSF_BUS_DELAY 150 /* assume a fix propagation in us  */
 #define TSMAX  1000        /* max no. of timing record kept   */
@@ -609,22 +605,22 @@ static void dhd_set_packet_filter(int value, dhd_pub_t *dhd)
 }
 
 #if defined(CONFIG_HAS_EARLYSUSPEND)
+static int wifi_pm = 0;
+module_param(wifi_pm, int, 0755);
+
 static int dhd_set_suspend(int value, dhd_pub_t *dhd)
 {
-#ifndef CUSTOMER_HW_SAMSUNG
-	int power_mode = PM_MAX;
-	
-	if ( wifi_pm == 1 )
-		power_mode = PM_FAST;
-	
+	int power_mode = PM_MAX;	
 	/* wl_pkt_filter_enable_t	enable_parm; */
-	char iovbuf[32];
-	int bcn_li_dtim = 3;
-	uint roamvar = 1;
-#endif
+	//char iovbuf[32];
+	//int bcn_li_dtim = 3;
+	//uint roamvar = 1;
 
 	DHD_ERROR(("%s: enter, value = %d in_suspend=%d\n",
 		__FUNCTION__, value, dhd->in_suspend));
+
+	if ( wifi_pm == 1 )
+		power_mode = PM_FAST;
 
 	if (dhd && dhd->up) {
 		if (value && dhd->in_suspend) {
